8e181b66d7158798751652023dd9981927055cbc
Manoj-R-Divakar <manojrdjnnce@gmail.com>
2026-03-23 00:00:00 +0530

    2.1.0 - Password encoded

--- src/main/java/com/inventory/backend/auth/service/AuthService.java
+++ src/main/java/com/inventory/backend/auth/service/AuthService.java
@@ -4,11 +4,15 @@ import com.inventory.backend.auth.entity.User;
 import com.inventory.backend.auth.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class AuthService {
     @Autowired
     private AuthRepository repo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public String registerUser(SignupRequest request) {
 
@@ -21,8 +25,7 @@ public class AuthService {
         User user = new User();
         user.setName(request.getName());
         user.setEmail(request.getEmail());
-        user.setPassword(request.getPassword()); // (we'll encrypt later)
-
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
         repo.save(user);
 
         return "User registered successfully";
